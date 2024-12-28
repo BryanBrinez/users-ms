83435ab6901170cc659a899aa82204384cb2a8a6
Bryan Brinez <bryan-26-8@hotmail.com>
2024-12-28 18:56:53 -0500

    Initial commit for users-ms

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsBoolean, IsEmail, IsOptional, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  
+    @IsString()
+    public name: string;  // Nombre del usuario
+  
+    @IsEmail()
+    public email: string;  // Correo electrónico del usuario
+
+   
+    @IsString()
+    @IsStrongPassword()
+    password: string;
+
+  
+    @IsBoolean()
+    @IsOptional()
+    public status?: boolean;  // Estado del usuario, por defecto true (activo)
+  
+  }
